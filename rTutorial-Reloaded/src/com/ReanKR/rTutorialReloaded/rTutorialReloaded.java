2347745054459355f5edacd5f82dc53f5eccb82a
ReanKR <whitehack97@gmail.com>
2016-02-12 13:21:03 +0900

    New Released

--- rTutorial-Reloaded/src/com/ReanKR/rTutorialReloaded/rTutorialReloaded.java
+++ rTutorial-Reloaded/src/com/ReanKR/rTutorialReloaded/rTutorialReloaded.java
@@ -2,10 +2,14 @@ package com.ReanKR.rTutorialReloaded;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import net.milkbowl.vault.economy.Economy;
 
 public class rTutorialReloaded extends JavaPlugin implements Listener
 {
@@ -25,17 +29,27 @@ public class rTutorialReloaded extends JavaPlugin implements Listener
 	public static boolean RewardItems = true;
 	public static List<String> ResultCommands;
 	public static List<ItemStack> ResultItems;
-	
-	// 
+
+	// Saved variable loaded by Location.yml
+	public static List<String> LocationMethod; // World,X,Y,Z,Pitch,Yaw
+	public static List<String> MessageMethod; // Main message, Sub message
+	public static int MethodAmount = 0; // Location method amount
+
+	// rTutorial Reloaded main variable
+	public static List<String> ErrorReporting; // Save error collection
 	public static rTutorialReloaded rTutorialReloaded;
 	public static String Prefix = "」e[」9r」aT」futorial」e]」f ";
+	public static Economy Eco;
 	public static Plugin plugin;
 	
 	//
+	private ConsoleCommandSender Server = Bukkit.getConsoleSender();
+	
 	@Override
 	public void onEnable()
 	{
-		
+		Server.sendMessage(Prefix + "」bM」fade 」bb」fy Rean KR,」9 whitehack97@gmail.com");
+		Server.sendMessage(Prefix + "」bD」fevoloper 」bW」febsite 」e: 」fhttp://cafe.naver.com/suserver24");
 	}
 	
 	@Override
